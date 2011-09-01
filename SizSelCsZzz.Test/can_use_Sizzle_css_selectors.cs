72a2e929fa10e49cdedafa352cb1edfe97371e7b
fschwiet <fschwiet@gmail.com>
2011-08-31 18:47:07 -0700

    an unimplemented test

---
+++ SizSelCsZzz.Test/can_use_Sizzle_css_selectors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+
+namespace SizSelCsZzz.Test
+{
+    public class can_use_Sizzle_css_selectors : GivenWhenThenFixture
+    {
+        public override void Specify()
+        {
+            given("a server serving a simple page", delegate
+            {
+                when("we try to find an element using a Sizzler specific selector", delegate
+                {
+                    then("it works");
+                });
+            });
+        }
+    }
+}
