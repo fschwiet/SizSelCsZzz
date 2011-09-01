034558b9668418f170bf3a5ee7eb5b3e8ed3493b
fschwiet <fschwiet@gmail.com>
2011-08-31 18:57:32 -0700

    a build script

--- SizSelCsZzz/Properties/AssemblyInfo.cs
+++ SizSelCsZzz/Properties/AssemblyInfo.cs
@@ -1,36 +1,20 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SizSelCsZzz")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SizSelCsZzz")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+#if !SILVERLIGHT
+[assembly: SuppressIldasmAttribute()]
+#endif
+[assembly: CLSCompliantAttribute(false )]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute("SizSelCsZzz 0.0.1.0")]
+[assembly: AssemblyDescriptionAttribute("An extension to Selenium to support Sizzler based CSS selectors.  Also, an extension method for waiting.")]
+[assembly: AssemblyCompanyAttribute("n/a")]
+[assembly: AssemblyProductAttribute("SizSelCsZzz 0.0.1.0")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2011")]
+[assembly: AssemblyVersionAttribute("0.0.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.0.1.0 / 0000000")]
+[assembly: AssemblyFileVersionAttribute("0.0.1.0")]
+[assembly: AssemblyDelaySignAttribute(false)]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("12faa10f-719a-4ec0-be40-e6f2af1f38cb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ default.ps1
@@ -0,0 +1,96 @@
+properties {
+    $baseDirectory  = resolve-path .
+    $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
+    $version = "0.0.1"
+
+    $shortDescription = "An extension to Selenium to support Sizzler based CSS selectors.  Also, an extension method for waiting."
+}
+
+import-module .\tools\PSUpdateXML.psm1
+. .\psake_ext.ps1
+
+task default -depends Build,RunTests,BuildNuget
+
+task Cleanup {
+    if (test-path $buildDirectory) {
+        rm $buildDirectory -recurse
+    }
+}
+
+task GenerateAssemblyInfo {
+	
+	$projectFiles = ls -path $base_dir -include *.csproj -recurse
+
+    $projectFiles | write-host
+	foreach($projectFile in $projectFiles) {
+		
+		$projectDir = [System.IO.Path]::GetDirectoryName($projectFile)
+		$projectName = [System.IO.Path]::GetFileName($projectDir)
+		$asmInfo = [System.IO.Path]::Combine($projectDir, [System.IO.Path]::Combine("Properties", "AssemblyInfo.cs"))
+				
+		Generate-Assembly-Info `
+			-file $asmInfo `
+			-title "$projectName $version.0" `
+			-description $shortDescription `
+			-company "n/a" `
+			-product "SizSelCsZzz $version.0" `
+			-version "$version.0" `
+			-fileversion "$version.0" `
+			-copyright "Copyright © Frank Schwieterman 2011" `
+			-clsCompliant "false"
+	}
+}
+
+task Build -depends Cleanup,GenerateAssemblyInfo {
+    $v4_net_version = (ls "$env:windir\Microsoft.NET\Framework\v4.0*").Name
+    $dearlySolution = "$baseDirectory\dearly.sln"
+
+    exec { &"C:\Windows\Microsoft.NET\Framework\$v4_net_version\MSBuild.exe" SizSelCsZzz.sln /T:"Clean,Build" /property:OutDir="$buildDirectory\" }    
+}
+
+task RunTests {
+    exec { & "$baseDirectory\packages\NUnit.2.5.10.11092\tools\nunit-console.exe" "$buildDirectory\SizSelCsZzz.Test.dll" -xml:"$buildDirectory\TestResults.xml" }
+}
+
+task BuildNuget -depends Build {
+
+    $nugetTarget = "$buildDirectory\nuget"
+
+    $null = mkdir "$nugetTarget\lib\"
+    $null = mkdir "$nugetTarget\tools\"
+
+    cp "$buildDirectory\SizSelCsZzz.dll" "$nugetTarget\lib\"
+    cp "$buildDirectory\SizSelCsZzz.pdb" "$nugetTarget\lib\"
+
+    $old = pwd
+    cd $nugetTarget
+
+    ..\..\tools\nuget.exe spec -a ".\lib\SizSelCsZzz.dll"
+
+    update-xml "SizSelCsZzz.nuspec" {
+
+        add-xmlnamespace "ns" "http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd"
+
+        for-xml "//ns:package/ns:metadata" {
+            set-xml -exactlyOnce "//ns:version" "$version.0"
+            set-xml -exactlyOnce "//ns:owners" "fschwiet"
+            set-xml -exactlyOnce "//ns:authors" "Frank Schwieterman"
+            set-xml -exactlyOnce "//ns:description" $shortDescription
+
+            set-xml -exactlyOnce "//ns:licenseUrl" "https://github.com/fschwiet/SizSelCsZzz/blob/master/LICENSE.txt"
+            set-xml -exactlyOnce "//ns:projectUrl" "https://github.com/fschwiet/SizSelCsZzz/"
+            remove-xml -exactlyOnce "//ns:iconUrl"
+            set-xml -exactlyOnce "//ns:tags" "Selenium, WebDriver, Browser, Automation"
+
+            set-xml -exactlyOnce "//ns:dependencies" ""
+            #append-xml -exactlyOnce "//ns:dependencies" "<dependency id=`"Newtonsoft.Json`" version=`"4.0`" />"
+            #append-xml -exactlyOnce "//ns:dependencies" "<dependency id=`"NDesk.Options`" version=`"0.2`" />"
+
+            append-xml "." "<summary>$shortDescription  This library requires ASP.NET 4.</summary>"
+        }
+    }
+
+    ..\..\tools\nuget pack "SizSelCsZzz.nuspec"
+
+    cd $old
+}
