7ac8bb86030c3d33c9f041272ff6e1444ae958d7
fschwiet <fschwiet@gmail.com>
2011-09-16 00:06:25 -0700

    updating version

--- default.ps1
+++ default.ps1
@@ -1,7 +1,7 @@
 properties {
     $baseDirectory  = resolve-path .
     $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
-    $version = "0.0.14"
+    $version = "0.0.15"
 
     $shortDescription = "An extension to Selenium to support Sizzle based CSS selectors.  Also, an extension method for waiting."
 }
