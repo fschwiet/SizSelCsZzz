6553f3446ba06edb25ee4ad3d24adf18dac5e5e7
fschwiet <fschwiet@gmail.com>
2011-10-05 21:43:29 -0700

    run all the firefoxes

--- SizSelCsZzz.Test/SpecificationForAllBrowsers.cs
+++ SizSelCsZzz.Test/SpecificationForAllBrowsers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using NJasmine;
@@ -17,21 +18,50 @@ namespace SizSelCsZzz.Test
 
         public sealed override void Specify()
         {
-            given("using Firefox", delegate
+            var browserRoot = arrange(() => GetBrowsersPath());
+
+            var allFirefoxVersions = new [] {"7.0.1", "6.0.2", "5.0.1"};
+            var firstFirefoxVersion = allFirefoxVersions.First();
+
+            given("Firefox " + firstFirefoxVersion + " with Firebug", delegate
             {
-                var browser = arrange(() => new FirefoxDriver());
+                withCategory("CommitTest");
+
+                var exePath = GetFirefoxExe(browserRoot, firstFirefoxVersion);
+                var xpiPath = GetFirebugXpi(browserRoot, firstFirefoxVersion);
+
+                expect(() => File.Exists(exePath));
+
+                var firefoxProfile = new FirefoxProfile();
+                arrange(() => firefoxProfile.AddExtension(xpiPath));
+                var browser = arrange(() => new FirefoxDriver(new FirefoxBinary(exePath), firefoxProfile));
 
                 SpecifyForBrowser(browser);
             });
 
-            given("using Chrome", delegate
+            if (Properties.Settings.Default.CommitTestsOnly)
+                return;
+
+            foreach(string version in allFirefoxVersions)
+            given("Firefox " + version, delegate
+            {
+                var exePath = GetFirefoxExe(browserRoot, firstFirefoxVersion);
+
+                expect(() => File.Exists(exePath));
+
+                var browser = arrange(() => new FirefoxDriver(new FirefoxBinary(exePath), new FirefoxProfile()));
+
+                SpecifyForBrowser(browser);
+            });
+
+            given("Chrome", delegate
             {
                 var browser = arrange(() => new ChromeDriver());
 
                 SpecifyForBrowser(browser);
             });
 
-            given("using Internet Explorer", delegate
+            given("Internet Explorer", delegate
             {
                 //this.ignoreBecause("InternetExplorerDriver requires particular security options.  Tools->Options->Security: 'Protected Mode' checkbox must match for all zones.");
 
@@ -46,6 +76,28 @@ namespace SizSelCsZzz.Test
 
                 SpecifyForBrowser(browser);
             });
+        }
+
+        string GetFirebugXpi(string browserRoot, string firstFirefoxVersion)
+        {
+            return arrange(() => Directory.GetFiles(Path.Combine(browserRoot, "firefox" + firstFirefoxVersion), "*.xpi").Single());
+        }
+
+        string GetFirefoxExe(string browserRoot, string firstFirefoxVersion)
+        {
+            return arrange(() => Path.Combine(browserRoot, "firefox" + firstFirefoxVersion + "\\firefox.exe"));
+        }
+
+        string GetBrowsersPath()
+        {
+            var rootPath = new FileInfo(new Uri(this.GetType().Assembly.CodeBase).LocalPath).Directory;
+            
+            while (!Directory.Exists(Path.Combine(rootPath.FullName, "browser_archive")))
+            {
+                rootPath = rootPath.Parent;
+            }
+
+            return Path.Combine(rootPath.FullName, "browser_archive");
         }
 
         public abstract void SpecifyForBrowser(IWebDriver browser);
