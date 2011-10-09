b2c603cd969e40e3821fe2b4b3040b8eab34b314
fschwiet <fschwiet@gmail.com>
2011-10-09 11:58:19 -0700

    trying newtonsoft as specific as selenium's

--- default.ps1
+++ default.ps1
@@ -1,7 +1,7 @@
 properties {
     $baseDirectory  = resolve-path .
     $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
-    $version = "0.0.19"
+    $version = "0.0.20"
     $releaseNotes = "Built against Selenium Webdriver 2.8"
 
     $browserArchiveDirectory="$baseDirectory\browser_archive"
@@ -99,7 +99,7 @@ task BuildNuget -depends RunTests {
             set-xml -exactlyOnce "//tags" "Selenium WebDriver Browser Automation"
 
             set-xml -exactlyOnce "//dependencies" ""
-            append-xml -exactlyOnce "//dependencies" "<dependency id=`"Newtonsoft.Json`" version=`"4.0`" />"
+            append-xml -exactlyOnce "//dependencies" "<dependency id=`"Newtonsoft.Json`" version=`"4.0.2`" />"
             append-xml -exactlyOnce "//dependencies" "<dependency id=`"Selenium.WebDriver`" version=`"2.6`" />"
             append-xml -exactlyOnce "//dependencies" "<dependency id=`"Selenium.Support`" version=`"2.6`" />"
 
