4c13baf3e09dd7c73d803385e42e7cdf4a591d46
fschwiet <fschwiet@gmail.com>
2011-10-16 00:12:32 -0700

    updating version

--- default.ps1
+++ default.ps1
@@ -1,7 +1,7 @@
 properties {
     $baseDirectory  = resolve-path .
     $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
-    $version = "0.0.22"
+    $version = "0.0.23"
 	$seleniumVersion = "2.8"
 
     $browserArchiveDirectory="$baseDirectory\browser_archive"
