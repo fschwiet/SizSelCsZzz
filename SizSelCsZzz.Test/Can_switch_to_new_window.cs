81b83a080a8c496dcc37b7490af0701f9a9e6c7e
fschwiet <fschwiet@gmail.com>
2011-12-19 14:11:27 -0800

    added some code to find new browser windows

---
+++ SizSelCsZzz.Test/Can_switch_to_new_window.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using SizSelCsZzz.Extras;
+
+namespace SizSelCsZzz.Test
+{
+    public class Can_switch_to_new_window : SpecificationForAllBrowsers
+    {
+        public override void SpecifyForBrowser(IWebDriver browser)
+        {
+            given("a webpage that opens another window", delegate()
+            {
+                var server = beforeAll(() => new StaticServer()
+                {
+                    {"first.html", "<html><a href='/second.html' target='_blank'>hi</a></html>"},
+                    {"second.html", "<html>new window here</html>"}
+                }.Start());
+
+                beforeAll(() => server.Start());
+
+                arrange(() => browser.Navigate().GoToUrl(server.UrlFor("first.html")));
+
+                var existingWindows = arrange(() => new ExistingWindow(browser));
+
+                when("the new window link is clicked", delegate()
+                {
+                    arrange(() => browser.FindElement(By.TagName("a")).Click());
+
+                    then("a new window is opened", delegate()
+                    {
+                        browser.SwitchTo().Window(existingWindows.GetNewWindowName());
+
+                        expect(() => browser.ContainsText("new window here"));
+                    });
+                });
+            });
+        }
+    }
+}
---
+++ SizSelCsZzz.Test/ExistingWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+
+namespace SizSelCsZzz.Test
+{
+    public class ExistingWindow
+    {
+        private readonly IWebDriver _browser;
+        private readonly List<string> _existingWindows;
+
+        public ExistingWindow(IWebDriver browser)
+        {
+            _browser = browser;
+
+            _existingWindows = new List<string>(browser.WindowHandles);
+        }
+
+        public string GetNewWindowName()
+        {
+            var currentWindows = _browser.WindowHandles;
+
+            var newWindows = currentWindows.Where(w => !_existingWindows.Contains(w));
+
+            var resultCount = newWindows.Count();
+
+            if (resultCount == 0)
+                return null;
+            else if (resultCount == 1)
+                return newWindows.Single();
+            else
+                throw new Exception("multiple new windows found");
+        }
+    }
+}
