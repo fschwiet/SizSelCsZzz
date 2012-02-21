add74028b5b7e2d41f8288405bd5f559e8005f65
fschwiet <fschwiet@gmail.com>
2012-02-20 18:10:40 -0800

    hmm, chromedriver wasn't closing without this

--- SizSelCsZzz.Test/SpecificationForAllBrowsers.cs
+++ SizSelCsZzz.Test/SpecificationForAllBrowsers.cs
@@ -73,6 +73,8 @@ namespace SizSelCsZzz.Test
                     });
                 });
 
+                cleanup(() => browser.Quit());
+
                 SpecifyForBrowser(browser);
             });
 
