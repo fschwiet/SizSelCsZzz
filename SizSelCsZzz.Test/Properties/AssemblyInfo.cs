11a9767a962a237126ad6ccea626080e56c49679
fschwiet <fschwiet@gmail.com>
2012-04-04 01:56:36 -0700

    version update

--- SizSelCsZzz.Test/Properties/AssemblyInfo.cs
+++ SizSelCsZzz.Test/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: CLSCompliantAttribute(false )]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("SizSelCsZzz.Test 0.1.4.0")]
+[assembly: AssemblyTitleAttribute("SizSelCsZzz.Test 0.1.5.0")]
 [assembly: AssemblyDescriptionAttribute("An extension to Selenium to support Sizzle based CSS selectors.  Also, an extension method for waiting.")]
 [assembly: AssemblyCompanyAttribute("n/a")]
-[assembly: AssemblyProductAttribute("SizSelCsZzz 0.1.4.0")]
+[assembly: AssemblyProductAttribute("SizSelCsZzz 0.1.5.0")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2011")]
-[assembly: AssemblyVersionAttribute("0.1.4.0")]
-[assembly: AssemblyInformationalVersionAttribute("0.1.4.0 / 0000000")]
-[assembly: AssemblyFileVersionAttribute("0.1.4.0")]
+[assembly: AssemblyVersionAttribute("0.1.5.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.1.5.0 / 0000000")]
+[assembly: AssemblyFileVersionAttribute("0.1.5.0")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
--- default.ps1
+++ default.ps1
@@ -1,7 +1,7 @@
 properties {
     $baseDirectory  = resolve-path .
     $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
-    $version = "0.1.4"
+    $version = "0.1.5"
 	$seleniumVersion = "2.20"
 
     $browserArchiveDirectory="$baseDirectory\browser_archive"
