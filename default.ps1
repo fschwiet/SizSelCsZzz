3b12f63beca149f74cf89ae24f1b9b1fe968e557
fschwiet <fschwiet@gmail.com>
2012-04-29 17:48:33 -0700

    increment version

--- default.ps1
+++ default.ps1
@@ -1,7 +1,7 @@
 properties {
     $baseDirectory  = resolve-path .
     $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
-    $version = "0.2.1"
+    $version = "0.2.2"
 	$seleniumVersion = "2.21"
 
     $browserArchiveDirectory="$baseDirectory\browser_archive"
