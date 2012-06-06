b6a7d844b94f9f4ab83cd3de5cd9c3167869fa65
fschwiet <fschwiet@gmail.com>
2012-06-06 10:34:21 -0700

    running tests against firefox 13

--- SizSelCsZzz.Test/SpecificationForAllBrowsers.cs
+++ SizSelCsZzz.Test/SpecificationForAllBrowsers.cs
@@ -23,7 +23,7 @@ namespace SizSelCsZzz.Test
         {
             var browserRoot = arrange(() => Properties.Settings.Default.BrowserArchivePath);
 
-            var allFirefoxVersions = new [] {"11.0", "8.0", "7.0.1", "6.0.2", "5.0.1"};
+            var allFirefoxVersions = new [] {"13.0", "11.0", "8.0", "7.0.1", "6.0.2", "5.0.1"};
             var firstFirefoxVersion = allFirefoxVersions.First();
 
             var allChromeVersions = new[] { "18.0.1025.142", "14.0.835.202", "13.0.782.220", "12.0.742.112" };
