bd0092faf8902a8b05f2e5917a1a97a0a02ca135
fschwiet <fschwiet@gmail.com>
2013-03-05 16:56:02 -0800

    updating version

--- SizSelCsZzz.Test/Properties/AssemblyInfo.cs
+++ SizSelCsZzz.Test/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: CLSCompliantAttribute(false )]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("SizSelCsZzz.Test 0.3.32.0")]
+[assembly: AssemblyTitleAttribute("SizSelCsZzz.Test 0.3.33.0")]
 [assembly: AssemblyDescriptionAttribute("Various utility methods for Selenium from .NET.  (Use jQuery or Sizzle css selectors, wait on ajax operations, check client-side exceptions, switch to/from new window)")]
 [assembly: AssemblyCompanyAttribute("n/a")]
-[assembly: AssemblyProductAttribute("SizSelCsZzz 0.3.32.0")]
+[assembly: AssemblyProductAttribute("SizSelCsZzz 0.3.33.0")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2011")]
-[assembly: AssemblyVersionAttribute("0.3.32.0")]
-[assembly: AssemblyInformationalVersionAttribute("0.3.32.0 / eb5c027")]
-[assembly: AssemblyFileVersionAttribute("0.3.32.0")]
+[assembly: AssemblyVersionAttribute("0.3.33.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.3.33.0 / a1f5b38")]
+[assembly: AssemblyFileVersionAttribute("0.3.33.0")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
--- default.ps1
+++ default.ps1
@@ -1,7 +1,7 @@
 properties {
     $baseDirectory  = resolve-path .
     $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
-    $version = "0.3.32"
+    $version = "0.3.33"
 	$seleniumVersion = "2.31"
 
     $browserArchiveDirectory="$baseDirectory\browser_archive"
