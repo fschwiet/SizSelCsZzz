df88f2d7904ebef570e7b4aa3e033f564764b13c
Frank <fschwiet@gmail.com>
2014-01-02 21:06:48 -0800

    update all the things

--- packages/phantomjs.exe.1.8.1/tools/uninstall.ps1
+++
@@ -1,5 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$file = Join-Path (Join-Path $toolsPath 'phantomjs') 'phantomjs.exe' | Get-ChildItem
-
-$project.ProjectItems.Item($file.Name).Delete()
